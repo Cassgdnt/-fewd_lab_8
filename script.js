4656690cef714e08b48e0a894ec41f4637af3715
Cassandre <cassandre.gaudinat@gmail.com>
2020-11-16 21:36:38 +0000

    fewd_lab_8

---
+++ script.js
@@ -0,0 +1,10 @@
+var planets = new Array("Mars", "Earth", "Jupiter", "Saturn", "Uranus");
+console.log(planets)
+
+
+var Hello = function(){
+    return "Hello World";
+}
+
+var result= Hello();
+console.log(Hello())
